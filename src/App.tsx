f941dde1625e82df229545c4ebf7de6341edce98
Bruno Cardoso <brunocardoso.developer@gmail.com>
2024-11-13 12:33:56 -0300

    style: add tailwindcss

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
-import React from "react";
+import React from 'react'
+import { Title } from './components/title'
+import './styles/global.css'
 
 function App() {
   return (
     <div className="App">
       <div className="container">This is my news!</div>
+      <Title size="large" variant="primary" />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
